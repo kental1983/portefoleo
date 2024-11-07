cb8b3d4768dfddaee976fcba4729680add58b10a
kental1983 <109003944+kental1983@users.noreply.github.com>
2024-11-07 20:49:37 +0000

    Add files via upload
    Signed-off-by: kental1983 <109003944+kental1983@users.noreply.github.com>

--- calculadora.php
+++ calculadora.php
@@ -223,8 +223,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ?>
         </div>
     </div>
-    <script src="scrip/time.js"></script>
-    <script src="scrip/scroolbar.js"></script>
+    <script src="script/time.js"></script>
+    <script src="script/scroolbar.js"></script>
 </body>
 </html>
 
