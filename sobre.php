345c47627657291214cdf3f5202f2c43eeb96030
kental1983 <109003944+kental1983@users.noreply.github.com>
2025-03-22 14:49:06 +0000

    Delete sobre.php
    Signed-off-by: kental1983 <109003944+kental1983@users.noreply.github.com>

--- sobre.php
+++
@@ -1,5 +0,0 @@
-<section id="sobre">
-<link rel="stylesheet" href="style.css">
-    <h2>Sobre Mim</h2>
-    <p>Olá! Eu sou João Quental, um desenvolvedor web com bases em html, javascipt, php e mysql. Tenho paixão pela informatica gosto de desenvolver e de meter em pratica as minhas ideias</p>
-</section>
